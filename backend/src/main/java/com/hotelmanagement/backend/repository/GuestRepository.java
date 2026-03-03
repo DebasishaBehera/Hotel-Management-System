6b4673e1fa536651f389b196bd90995b9f59f4b1
Debasisha Behera <debasisha.behera.dev@gmail.com>
2026-03-03 17:38:33 +0530

    Delete backend directory

--- backend/src/main/java/com/hotelmanagement/backend/repository/GuestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotelmanagement.backend.repository;
-
-import com.hotelmanagement.backend.entity.Guest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GuestRepository extends JpaRepository<Guest, Long> {
-}
